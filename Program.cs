50af8807892dbfd55c2c2871af984593e5fd7c27
Tandi Sunarto <tandi.sunarto@vaeit.com>
2021-06-22 10:14:36 -0400

    Add env variables to the configurationbuilder

--- Program.cs
+++ Program.cs
@@ -72,7 +72,8 @@ namespace ScarletAuth.AdminUI.Admin
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
                 .AddJsonFile("serilog.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"serilog.{environment}.json", optional: true, reloadOnChange: true);
+                .AddJsonFile($"serilog.{environment}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
             if (isDevelopment)
             {
